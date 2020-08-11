08c8cbdb872a60e490c0f5368e17dc7c3455fa0f
steled <steled@gmx.net>
2020-08-11 19:34:59 +0200

    removed sudo and moved from vagrantfile

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
+chown vagrant:vagrant -R ~/.kube/
+
 cd /challenge/test
 
 export GOROOT=/usr/local/go-1.14
 export PATH=$GOROOT/bin:$PATH
 
-sudo go mod init github.com/steled/challenge01
+go mod init github.com/steled/challenge01
 
-sudo go test -v kubernetes_example_app_test.go
+go test -v kubernetes_example_app_test.go
